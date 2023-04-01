1eba71af5c4d12b0c9eed01f18d6358d13e230dc
Miquéias Coelho <miqueias.pereira.coelho@gmail.com>
2023-04-01 17:14:10 -0300

    increaseFiles
    -Recebendo Props
    -Mudança dinâmica de conteúdo nas tags
    -Alterando links de requisição dos videos e vinil

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
+        <Video 
+          likes={10}
+          messages={20}
+          shares={30}
+          name="micas"
+          description="Gato goleiro"
+          music="música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+          likes={100}
+          messages={2000}
+          shares={300}
+          name="nerd"
+          description="gato músico"
+          music="clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
+
       </div>
     </div>
   );
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
 import "./video.css";
 import VideoFooter from "./components/footer/VideoFooter";
+import VideoSidebar from "./components/sidebar/VideoSidebar";
 
-function Video() {
+function Video({likes, messages, shares, name, description, music,url}) {
   const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
 
@@ -23,10 +24,18 @@ function Video() {
         ref={videoRef}
         onClick={handdleStart}
         loop
-        src="https://firebasestorage.googleapis.com/v0/b/jornada-b5e9d.appspot.com/o/brecker2.mp4?alt=media&token=9aa979e9-0907-4093-abd5-a71efe4fc946"
+        src={url}
       ></video>
-      {/*Side bar */}
-      <VideoFooter/>
+      <VideoSidebar 
+        likes={likes}
+        messages={messages}
+        shares={shares}
+      /> 
+      <VideoFooter
+        name={name}
+        description={description}
+        music={music}
+      />
     </div>
   );
 }
